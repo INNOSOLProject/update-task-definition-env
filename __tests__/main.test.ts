11cee41e76ea11767ad61cb926985980a6b658e7
Ivan Greene <ivan@ivan.sh>
2020-08-28 14:18:50 -0500

    Run formatting, fix linting errors, build dist

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,32 +1,32 @@
-import { updateTaskDefinitionFile } from '../src/update-task-definition';
-import { file } from 'tmp-promise';
-import fs from 'fs';
+import {updateTaskDefinitionFile} from '../src/update-task-definition'
+import {file} from 'tmp-promise'
+import fs from 'fs'
 
 test('updates task def', async () => {
   const variables = {
     FOO: 'bar',
-    ELASTICSEARCH_URL: 'localhost:9200',
-  } as any;
+    ELASTICSEARCH_URL: 'localhost:9200'
+  } as any
 
-  const { path, cleanup } = await file();
+  const {path, cleanup} = await file()
 
-  const testData = await fs.promises.readFile('__tests__/sample-task-def.json');
+  const testData = await fs.promises.readFile('__tests__/sample-task-def.json')
 
-  await fs.promises.writeFile(path, testData);
+  await fs.promises.writeFile(path, testData)
 
-  await updateTaskDefinitionFile(path, variables);
+  await updateTaskDefinitionFile(path, variables)
 
-  const resultDef = JSON.parse(await fs.promises.readFile(path, 'utf-8'));
+  const resultDef = JSON.parse(await fs.promises.readFile(path, 'utf-8'))
 
-  expect(resultDef.containerDefinitions.length).toEqual(3);
+  expect(resultDef.containerDefinitions.length).toEqual(3)
 
   resultDef.containerDefinitions.forEach((containerDef: any) => {
-      expect(containerDef.environment.length).toEqual(2);
-      containerDef.environment.forEach((envVar: any) => {
-        expect(variables.hasOwnProperty(envVar.name)).toEqual(true);
-        expect(envVar.value).toEqual(variables[envVar.name]);
-      })
+    expect(containerDef.environment.length).toEqual(2)
+    containerDef.environment.forEach((envVar: any) => {
+      expect(variables.hasOwnProperty(envVar.name)).toEqual(true)
+      expect(envVar.value).toEqual(variables[envVar.name])
     })
-  
-  cleanup();
+  })
+
+  cleanup()
 })
